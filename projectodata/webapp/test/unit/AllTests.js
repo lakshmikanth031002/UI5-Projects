5c50ffe25c542f5842a6a858a7d2760e4ea16598
lakshmikanth S <sankarkaran2002@gmail.com>
2025-07-18 05:34:59 +0000

    New Update

--- projectodata/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"projectodata/test/unit/controller/FirstView.controller"
-], function () {
-	"use strict";
-});
---
+++ ui5assessment/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ui5assessment/test/unit/controller/Material.controller"
+], function () {
+	"use strict";
+});
