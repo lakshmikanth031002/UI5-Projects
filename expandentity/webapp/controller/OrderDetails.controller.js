5d621b5c9d03e8446c3e18f034471cb71a2dbc18
lakshmikanth S <sankarkaran2002@gmail.com>
2025-07-31 09:50:10 +0000

    Final Update

--- expandentity/webapp/controller/OrderDetails.controller.js
+++ expandentity/webapp/controller/OrderDetails.controller.js
@@ -5,6 +5,17 @@ sap.ui.define([
 
     return Controller.extend("ladera.expandentity.controller.OrderDetails", {
         onInit() {
+            
+        },
+        onNavOrderItemView: function(oEvent){
+
+           var SelectedOrderId = oEvent.getSource().getBindingContext().getProperty().OrderID;
+
+           var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+           oRouter.navTo("RouteOrderDrillDownItem", 
+               { "OrderId": SelectedOrderId })
+    
+
         }
     });
 });
